d508b9cc4e3195a21249be05172b2a46198af588
Rahul-training <52369556+Rahul-training@users.noreply.github.com>
2024-06-13 18:52:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "azurerm" {
 
   subscription_id = var.subscription_id
-  tenant_id       = var.tenent_id
+  tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   features {}
